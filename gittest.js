5577af81896909e5f561de0fdc6c459d1e8e4dd7
MarcosVinDev <marcosviniciossl3@gmail.com>
2025-02-11 18:16:05 -0300

    for added in the log

--- gittest.js
+++ gittest.js
@@ -7,3 +7,7 @@ let user = prompt("Digite a senha/What's the pw??");
 if (user == "Hello World" || "hello world" || "Hello world" || "hello" || "Hello") {
     alert ("HELLO WORLD!!!!!!");
 }
+
+for (let i = 0; i < 10; i++ ) {
+    console.log(i);
+}
